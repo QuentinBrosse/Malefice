5ad0a03fd972a5284581d2ca5c4bbd8843a9bf3d
LaBretonne <sacha.carle@epitech.eu>
2016-05-03 11:53:21 +0200

    Modification on Factory.

--- Shared/PlayerFactory.cpp
+++ Shared/PlayerFactory.cpp
@@ -9,28 +9,25 @@
 #include "SpellManager.h"
 #include "Spell.h"
 
-namespace factory
+ecs::Entity* PlayerFactory::createPlayer(float x, float y, float z, unsigned int networkId, int team, int life)
 {
-	ecs::Entity PlayerFactory::createPlayer(float x, float y, float z, unsigned int networkId, int team, int life)
-	{
-		ecs::Entity	entity = ObjectFactory::createObject(x, y, z);
+	ecs::Entity* entity = ObjectFactory::createObject(x, y, z);
 
-		entity[ecs::LIFE] = new ecs::Life(life);
-		entity[ecs::NETWORK_ID] = new ecs::NetworkID(networkId);
-		entity[ecs::TEAM] = new ecs::Team(team);
-		entity[ecs::WEAPON_MANAGER] = new ecs::WeaponManager(ecs::Weapon("gun", 10, 5, 20, 50, ecs::Weapon::REVOLVER, true));
-		entity[ecs::MOVEMENT] = new ecs::Movement(ecs::Position(x, y, z));
-		entity[ecs::SPELL] = new ecs::Spell();
+	(*entity)[ecs::LIFE] = new ecs::Life(life);
+	(*entity)[ecs::NETWORK_ID] = new ecs::NetworkID(networkId);
+	(*entity)[ecs::TEAM] = new ecs::Team(team);
+	(*entity)[ecs::WEAPON_MANAGER] = new ecs::WeaponManager(ecs::Weapon("gun", 10, 5, 20, 50, ecs::Weapon::REVOLVER, true));
+	(*entity)[ecs::MOVEMENT] = new ecs::Movement(ecs::Position(x, y, z));
+	(*entity)[ecs::SPELL] = new ecs::Spell();
 
-		return entity;
-	}
+	return entity;
+}
 
-	ecs::Entity PlayerFactory::createPredator(float x, float y, float z, unsigned int networkId)
-	{
-		ecs::Entity entity = PlayerFactory::createPlayer(x, y, z, networkId, 2, 1000);
+ecs::Entity* PlayerFactory::createPredator(float x, float y, float z, unsigned int networkId)
+{
+	ecs::Entity* entity = PlayerFactory::createPlayer(x, y, z, networkId, 2, 1000);
 
-		entity[ecs::SPELL_MANAGER] = new ecs::SpellManager(ecs::Spell(ecs::Spell::CONFUSION));
-
-		return entity;
-	}
+	(*entity)[ecs::SPELL_MANAGER] = new ecs::SpellManager(ecs::Spell(ecs::Spell::CONFUSION));
+	
+	return entity;
 }
