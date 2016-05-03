e8ecbb8e248c3bb2d604dc6a98f75ab37f29f2d2
Tookie26 <david.galy@epitech.eu>
2016-05-03 10:52:33 +0200

    pull du matin

--- Shared/ClientCore.h
+++ Shared/ClientCore.h
@@ -2,6 +2,9 @@
 
 #include <map>
 #include "Entity.h"
+#include "MovementSystem.h"
+#include "WeaponSystem.h"
+#include "SpellSystem.h"
 
 class ClientCore
 {
@@ -12,7 +15,7 @@ public:
 	void	addEntity(ecs::Entity& newEntity);
 	void	deleteEntity(int id);
 	void	dump()	const;
-
+	void	run();
 private:
 	std::map<int, ecs::Entity&>	m_entities;
 };
