7c8550228ee3885ea605754827e7deeeb83b55f4
QuentinBross <quentinb8@gmail.com>
2016-05-24 19:15:47 +0200

    Merge

--- Server/Server.Core/WeaponSystem.cpp
+++ Server/Server.Core/WeaponSystem.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "WeaponSystem.h"
 #include "PhysicsUtil.h"
 #include "NodePickable.h"
@@ -51,26 +50,3 @@ namespace ecs
 		}
 	}
 }
-=======
-#include "WeaponSystem.h"
-
-namespace ecs
-{
-	void	WeaponSystem::shoot(Entity* entity, irr::core::line3df* ray, RakNet::RPC3* rpc)
-	{
-		WeaponManager*	weaponManager;
-
-		if ((weaponManager = dynamic_cast<WeaponManager*>((*entity)[ecs::AComponent::ComponentType::WEAPON_MANAGER])) != nullptr)
-		{
-			Weapon&	weapon = weaponManager->getCurrentWeapon();
-
-			weapon.shoot();
-			/*
-			 * TODO:
-			 * 1) Get the entity we shot (let's call it "shotEntity")
-			 * 2) If its life <= 0, call LifeSystem::die(shotEntity) (and LifeSystem::die itself call client's RPC die() or something)
-			*/
-		}
-	}
-}
->>>>>>> 5e7d056b5b7081dd18f0a16498f3b1c16f88dcd9
