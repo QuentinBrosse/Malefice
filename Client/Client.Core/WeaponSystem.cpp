d5e76b87f5d1e0be90432424611d46cf5435bc9f
QuentinBross <quentinb8@gmail.com>
2016-05-24 13:00:57 +0200

    Add Offset + CoefOffset FPSMetrics to weapon && Add getRay to WeaponSystem (client)

--- Client/Client.Core/WeaponSystem.cpp
+++ Client/Client.Core/WeaponSystem.cpp
@@ -2,10 +2,11 @@
 
 #include "WeaponSystem.h"
 #include "SceneAnimatedMesh.h"
+#include "GraphicUtil.h"
 
 namespace ecs
 {
-	void				WeaponSystem::shoot(Entity& entity, bool isExplosive)
+	void				WeaponSystem::shoot(Entity& entity)
 	{
 		WeaponManager*	weaponManager;
 
@@ -20,6 +21,28 @@ namespace ecs
 
 			//TODO: sens to msg to server
 		}
+	}
+
+	irr::core::line3df	WeaponSystem::getRay(Entity & entity)
+	{
+		WeaponManager*				weaponManager;
+		irr::core::line3d<irr::f32> ray;
+
+		if ((weaponManager = dynamic_cast<WeaponManager*>(entity[ecs::AComponent::ComponentType::WEAPON_MANAGER])) != nullptr)
+		{
+			Weapon&					weapon = weaponManager->getCurrentWeapon();
+			Camera*					camera = GraphicUtil::getInstance().getFPSCamera();
+
+			weapon.getScene()->getScene()->updateAbsolutePosition();
+
+			ray.start = weapon.getScene()->getScene()->getAbsolutePosition();
+			ray.end = ray.start + (camera->getTarget() - ray.start).normalize() * weapon.getDistance();
+			ray.start += weapon.getFPSMetricsOffset();
+			irr::core::vector3df vector(ray.end - ray.start);
+			vector.normalize();
+			ray.start += vector * weapon.getFPSMetricsCoefOffset();
+		}
+		return ray;
 	}
 
 }
