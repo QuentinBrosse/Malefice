994a54226505ee8ad72cd99abb4f68269b099b6a
bobis_b <brendan.bobis@epitech.eu>
2016-06-13 12:32:02 +0200

    hotfix

--- Client/Client.Core/LifeSystem.cpp
+++ Client/Client.Core/LifeSystem.cpp
@@ -1,6 +1,7 @@
 #include "LifeSystem.h"
 #include "Position.h"
 #include "GraphicUtil.h"
+#include "ClientCore.h"
 
 void	LifeSystem::die(RakNet::RPC3* rpc)
 {
@@ -9,4 +10,5 @@ void	LifeSystem::die(RakNet::RPC3* rpc)
 		irr::core::vector3df(50.0f, 50.0f, -60.0f),
 		irr::core::vector3df(-70.0f, 30.0f, -60.0f));
 	GraphicUtil::getInstance().getFPSCamera()->setPosition(cameraPosition);
+	ClientCore::getInstance().getGraphicModule()->getDeadGUI()->display();
 }
